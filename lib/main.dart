4886d75d06b41f17d3babbae784308ca9817add3
patrick <patrickchow0803@gmail.com>
2020-02-10 17:47:47 -0500

    Added 2 images to display the dice

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,26 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: <Widget>[
+        // Expanded makes it so that the image tries to strech as much as possible depending on the parent widget.
+        // For ex, since the parent widget is a Row, it tries to expand on the width as much as possible.
+        Expanded(
+          // Flex is used to determine how much space the Expanded widget should use.
+          // For example, if the first expanded widget has a flex value of 3, and the second expanded widget has a flex value of 2,
+          // The ratio would be a 3:2 size comparison.
+          flex: 1,
+          // Image.asset('images/dice2.png') is a short hand way of doing the commented code below.
+          child: Image.asset('images/dice2.png'),
+//          Image(
+//            image: AssetImage('images/dice2.png'),
+//          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Image.asset('images/dice2.png'),
+        ),
+      ],
+    );
   }
 }
